e50db6700017af0e02fd15f1433781be4539724c
201241 <77318315+201241@users.noreply.github.com>
2021-02-28 14:07:21 -0600

    creando tablas para dao

--- BD201241/src/simplehibernate/Cita.java
+++ BD201241/src/simplehibernate/Cita.java
@@ -1,77 +1,10 @@
 package simplehibernate;
 
-import javax.persistence.*;
+public class Cita{
 
-@Entity
-@Table(name = "Cita")
-public class Cita {
+    public Cita(String nombre, String sexo, int edad, int idCiudad){
 
-    @Id
-    @GeneratedValue
-    @Column(name = "idCita")
-    private int idCita;
-
-    @Column(name = "Fecha")
-    private String Fecha;
-
-    @Column(name = "Hora")
-    private String Hora;
-
-    @Column(name = "Servicio")
-    private int Servicio;
-
-    @Column(name = "idMascota")
-    private int idMascota;
-
-    public Cita() {
     }
 
-    public Cita(String Fecha, String Hora, int Servicio, int idMascota) {
-        this.Fecha = Fecha;
-        this.Hora = Hora;
-        this.Servicio = Servicio;
-        this.idMascota=idMascota; //objeto tipo mascota muchos a uno.
-    }
-
-
-    public String getHora() {
-        return Hora;
-    }
-
-    public void setHora(String hora) {
-        Hora = hora;
-    }
-
-    public int getServicio() {
-        return Servicio;
-    }
-
-    public void setServicio(int servicio) {
-        Servicio = servicio;
-    }
-
-    public int getIdMascota() {
-        return idMascota;
-    }
-
-    public void setIdMascota(int idMascota) {
-        this.idMascota = idMascota;
-    }
-
-    public String getFecha() {
-        return Fecha;
-    }
-
-    public void setFecha(String fecha) {
-        Fecha = fecha;
-    }
-
-    public int getIdCita() {
-        return idCita;
-    }
-
-    public void setIdCita(int idCita) {
-        this.idCita = idCita;
-    }
 
 }
